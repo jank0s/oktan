5d8847179451d500ecb69f1120f0ca01aec479f0
jan@k0s.si <jan@k0s.si>
2014-01-03 16:36:32 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Oktan::Application.config.secret_key_base = 'e22281e87472760418fea2ee0f3d7d1101d32b5d4bd65f1ef94de73abc83dc43c3c9bab57c7880dc4f1934b597006da1ca602960fa702eccc5ada99db365f894'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Oktan::Application.config.session_store :cookie_store, key: '_oktan_session'
