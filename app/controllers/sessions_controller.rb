ea51dec3d973bea1f1196f879a9cd6493d8dca75
jan@k0s.si <jan@k0s.si>
2014-01-04 13:35:46 +0100

    Reditect to home if login failed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,10 @@ class SessionsController < ApplicationController
 	def create
 	    user = User.find_by(email: params[:session][:email].downcase)
 	    if user && user.authenticate(params[:session][:password])
-	      sign_in user
-	      redirect_to user
+	    	sign_in user
+	    	redirect_to user
 	    else
-	      render 'new'
+	    	redirect_to root_url
 	    end
   	end
 
