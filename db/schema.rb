f63db88542a5a13595df0a870185029594bcf398
jan@k0s.si <jan@k0s.si>
2014-01-04 01:09:34 +0100

    User secure passwords

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140103234254) do
+ActiveRecord::Schema.define(version: 20140103235745) do
 
   create_table "makes", force: true do |t|
     t.string   "name"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20140103234254) do
 
   create_table "users", force: true do |t|
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
