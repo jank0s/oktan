f6c907c9a0cf4209492acc18efd3c162905566f7
jan@k0s.si <jan@k0s.si>
2014-01-04 23:21:09 +0100

    DB date and time

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,6 +3,7 @@ class Car < ActiveRecord::Base
 	belongs_to :model
 	belongs_to :fuel_type
 	has_many :entries
+	attr_accessor :test
 
 	validates :model_id, presence: true
 	validates :user_id, presence: true
@@ -10,6 +11,6 @@ class Car < ActiveRecord::Base
 	validates :power, presence: true
 	validates :fuel_type_id, presence: true
 
-	def avg
+	def calcAvg
 		return (self.entries.sum('quantity')-self.entries.order(:date).first.quantity)/(self.entries.maximum('odo')-self.entries.minimum('odo'))*100	end
 end
